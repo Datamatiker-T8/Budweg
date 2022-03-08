6112c6ceaca3462e2a7f6fbc10c8c57e30c2729b
Matt Blomgrén Mauritsen <mbma37913@edu.ucl.dk>
2022-03-08 10:58:41 +0100

    Update

--- Budweg/Views/MainWindow.xaml.cs
+++ Budweg/Views/MainWindow.xaml.cs
@@ -19,54 +19,11 @@ namespace Budweg
 {
     public partial class MainWindow : Window
     {
-        DispatcherTimer timer;
-
-        double panelWidth;
-        bool hidden;
+        
         public MainWindow()
         {
             InitializeComponent();
-            timer = new DispatcherTimer();
-            timer.Interval = new TimeSpan(0, 0, 0, 0, 8);
-            timer.Tick += Timer_Tick;
-
-            panelWidth = sidePanel.Width;
-        }
-
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            double Timepanelspeed = 1.5;
             
-            if (hidden)
-            {
-                sidePanel.Width += Timepanelspeed;
-                if (sidePanel.Width >= panelWidth)
-                {
-                    timer.Stop();
-                    hidden = false;
-                }
-            }
-            else
-            {
-                sidePanel.Width -= Timepanelspeed;
-                if (sidePanel.Width <= 50)
-                {
-                    timer.Stop();
-                    hidden = true;
-                }
-            }
-        }
-        private void Menu_Click(object sender, RoutedEventArgs e)
-        {
-            timer.Start();
-        }
-
-        private void PanelHeader_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                DragMove();
-            }
         }
 
         private void Overviewbt_Click(object sender, RoutedEventArgs e)
