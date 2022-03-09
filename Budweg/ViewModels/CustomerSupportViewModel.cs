bb6c804840c5238b2a560f6147476d03fdb74a5b
Phillip Friis-Alstrup <phillip.friis.alstrup@gmail.com>
2022-03-09 12:18:53 +0100

    Added CRUD for brakecaliber in CustomerSupportViewModel

--- Budweg/ViewModels/CustomerSupportViewModel.cs
+++ Budweg/ViewModels/CustomerSupportViewModel.cs
@@ -12,16 +12,100 @@ namespace Budweg.ViewModels
     public class CustomerSupportViewModel
     {
         BrakecaliberRepository brakeRepo = new BrakecaliberRepository();
-        public ObservableCollection<BrakeCaliber> BrakeCaliberList { get; set; }
-        public ObservableCollection<Ressource> RessourceList { get; set; }
-        public ObservableCollection<Feedback> FeedbackList { get; set; }
+        public ObservableCollection<BrakeCaliber> brakeCaliberList { get; set; }
+        public ObservableCollection<Ressource> ressourceList { get; set; }
+        public ObservableCollection<Feedback> feedbackList { get; set; }
 
         public CustomerSupportViewModel()
         {
-            
+            foreach (BrakeCaliber brake in brakeRepo.GetAll())
+            {
+                brakeCaliberList.Add(brake);
+            }
         }
 
-        public void AddCaliber() {}
+        // -------------------------------------------
+        //
+        // BRAKECALIBER
+        //
+        // -------------------------------------------
+
+        public void AddBrakeCaliber(string modelNumber, string linkQRCode) 
+        {
+            BrakeCaliber brakeCaliber = new BrakeCaliber(modelNumber, linkQRCode);
+            int addedCaliber = brakeRepo.Add(brakeCaliber);
+
+            brakeCaliberList.Add(brakeRepo.GetById(addedCaliber));
+        }
+
+        public BrakeCaliber GetBrakeCaliber(int id)
+        {
+            return brakeRepo.GetById(id);
+        }
+
+        public BrakeCaliber UpdateBrakeCaliber(int id)
+        {
+            return null;
+        }
+
+        public void DeleteBrakeCaliber(int id)
+        {
+
+        }
+
+        // -------------------------------------------
+        //
+        // RESOURCE
+        //
+        // -------------------------------------------
+
+        public void AddResource()
+        {
+
+        }
+
+        public Ressource GetResource(int id)
+        {
+            return null;
+        }
+
+        public Ressource UpdateResource(int id)
+        {
+            return null;
+        }
+
+        public void DeleteResource(int id)
+        {
+
+        }
+
+        // -------------------------------------------
+        //
+        // FEEDBACK
+        //
+        // -------------------------------------------
+
+        public void AddFeedback()
+        {
+
+        }
+
+        public Feedback GetFeedback(int id)
+        {
+            return null;
+        }
+
+        public Feedback UpdateFeedback(int id)
+        {
+            return null;
+        }
+
+        public void DeleteFeedback(int id)
+        {
+
+        }
+
+
 
     }
 }
